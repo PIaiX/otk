8e8f14c9a74da2064b6c72d5ed374f522edd932f
ngf93 <nabiullinaguzel93@gmail.com>
2023-05-24 18:29:13 +0300

    fix4

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import {Link} from 'react-router-dom';
-import HumanFull from '../assets/imgs/bg/human-full.png';
 import Offer from '../components/Offer';
 
 const Services = () => {
